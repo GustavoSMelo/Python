94e7c6b97f3a766989fb465f08ae8ac4efd0cfdf
Gustavo S. Melo <gsantos15569@gmail.com>
2020-11-11 19:16:45 -0300

    variables in python

---
+++ variables/python01.py
@@ -0,0 +1,5 @@
+old = int(input("Digite sua idade? "))
+name = str(input("Digite seu nome? "))
+salary = float(input("Quanto voce recebe? "))
+print(f'a idade de {name} é de: {old} anos\n'
+      f'E recebe {salary}R$ mensais')
